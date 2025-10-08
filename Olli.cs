5d19459b47286a18f4c16d93fbd4bb32a8a3b901
Oliver Lidberg <oliverlidberg@gmail.com>
2025-10-08 15:00:47 +0200

    added class olli

---
+++ Olli.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp8Test
+{
+    internal class Olli
+    {
+        public static void TestOlli()
+        {
+            Console.WriteLine("Heeej");
+        }
+    }
+}
