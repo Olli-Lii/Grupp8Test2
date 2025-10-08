75d92636c0ef755bc6473a0eae6727442457c164
Johan Heman <jojo2001@hotmail.se>
2025-10-08 15:02:17 +0200

    added hello

---
+++ Hej.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupp8Test
+{
+    internal class Hej
+    {
+
+        public static void hello()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+}
