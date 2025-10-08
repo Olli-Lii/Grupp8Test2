31305d99571a5e20d7229b505b7ef44979fb2dab
Oliver Lidberg <oliverlidberg@gmail.com>
2025-10-08 14:42:48 +0200

    New project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Grupp8Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
